2acb1d3b2983f6068709e211e63aeb25f9ae31ee
Liears <hobingwen@gmail.com>
2022-07-30 13:37:20 +0800

    斐波那契数列

--- 测试代码.cpp
+++ 测试代码.cpp
@@ -1,41 +1,16 @@
-class CQueue {
+class Solution {
 public:
-
-    CQueue() {
-        stack<int> stack1;
-        stack<int> stack2;
-
+    int fib(int n) {
+        int a = 0;b = 1;sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum = (a + b) % 1000000007;
+            a = b;
+            b = sum;
+        }
+        return a;
+        
+        
     }
     
-    void appendTail(int value) {
-        stack1.push(value);
-        
-    }
-    
-    int deleteHead() {
-        if (stack1.empty()) return -1;
-        while (!stack1.empty())
-        {
-            stack2.push(stack1.top());
-            stack1.pop();
-            /* code */
-        }
-        int res = stack2.top();
-        stack2.pop();
-        while (!stack2.empty())
-        {
-            stack2.pop();
-            stack1.push(stack2.top());
-        }
-        return res;
-        
-        
-    }
 };
-
-/**
- * Your CQueue object will be instantiated and called as such:
- * CQueue* obj = new CQueue();
- * obj->appendTail(value);
- * int param_2 = obj->deleteHead();
- */
