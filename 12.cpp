7f8d372abb7d663f546f221a16c3bae481f36f61
Liears <hobingwen@gmail.com>
2022-07-31 23:28:21 +0800

    剪绳子求最大值

--- 12.cpp
+++ 12.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int cuttingRope(int n) {
-        vector<int> f;
+        int f[n + 1];
         int ans = 1;
         if(n <= 3){
             return max(n-1,1);
@@ -11,11 +11,11 @@ public:
         f[2] = 2;
         f[3] = 3;
 
-        for (int i = 4; i < n; i++)
+        for (int i = 4; i <= n; i++)
         {
-            for (int j = 1; j < n; j++)
+            for (int j = 1; j <= i / 2; j++)
             {
-                ans = main(ans,f[j] * f[i-j]);
+                ans = max(ans,f[j] * f[i-j]);
             }
             f[i] = ans;
         }
