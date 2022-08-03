1cd062f45833a8fa794ef4a3ee125b330b7ff731
Liears <hobingwen@gmail.com>
2022-08-03 11:59:19 +0800

    正则表达式匹配 hard

---
+++ 正则表达式匹配.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.size() + 1, n = p.size() + 1;
+        vector<vector<bool>> dp(m, vector<bool>(n, false));
+        dp[0][0] = true;
+        // 初始化首行
+        for(int j = 2; j < n; j += 2)
+            dp[0][j] = dp[0][j - 2] && p[j - 1] == '*';
+        // 状态转移
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                if(p[j - 1] == '*') {
+                    if(dp[i][j - 2]) dp[i][j] = true;                              // 1.
+                    else if(dp[i - 1][j] && s[i - 1] == p[j - 2]) dp[i][j] = true; // 2.
+                    else if(dp[i - 1][j] && p[j - 2] == '.') dp[i][j] = true;      // 3.
+                } else {
+                    if(dp[i - 1][j - 1] && s[i - 1] == p[j - 1]) dp[i][j] = true;  // 1.
+                    else if(dp[i - 1][j - 1] && p[j - 1] == '.') dp[i][j] = true;  // 2.
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
+
